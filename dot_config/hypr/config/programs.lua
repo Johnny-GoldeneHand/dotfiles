31fe4ed1b8c1b7267c9cba713d706bc34ce2fa3a
Johnny-GoldeneHand <mnsener@github.com>
2026-05-12 23:48:07 +0200

    restructure hyprland and waybar config

---
+++ dot_config/hypr/config/programs.lua
@@ -0,0 +1,9 @@
+local programs = {
+    terminal      = "kitty",
+    fileManager   = "dolphin", 
+    menu          ="hyprlauncher", 
+    browser       = "firefox",
+    editor        = "vim",
+    mainMod       = "SUPER"
+}
+return programs
